e3a1ecfe5e2fc08fe117e160b61c4cf889fafa12
Prakhar Gupta <prakhag@clemson.edu>
2021-12-01 11:45:45 -0500

    Update organisational

--- include/deeporange_fort_driver/dispatch_FortRobotics.h
+++ include/deeporange_fort_driver/dispatch_FortRobotics.h
@@ -1,36 +1,5 @@
 /*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2018-2019 New Eagle 
- *  Copyright (c) 2015-2018, Dataspeed Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Dataspeed Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
+ Refer to FORT SRC Manual for description
  *********************************************************************/
 
 #ifndef _DO_FORT_DISPATCH_H
@@ -39,17 +8,17 @@
 
 namespace do_fortrobotics_can
 {
-typedef enum {
-  VIN_MUX_VIN0  = 0x00,
-  VIN_MUX_VIN1  = 0x01,
-  VIN_MUX_VIN2  = 0x02,
-} VinMux;
+// typedef enum {
+//   VIN_MUX_VIN0  = 0x00,
+//   VIN_MUX_VIN1  = 0x01,
+//   VIN_MUX_VIN2  = 0x02,
+// } VinMux;
 
-typedef enum {
-  WHEEL_SPEED_MUX0  = 0x00,
-  WHEEL_SPEED_MUX1  = 0x01,
-  WHEEL_SPEED_MUX2  = 0x02,
-} WheelSpeedMux;
+// typedef enum {
+//   WHEEL_SPEED_MUX0  = 0x00,
+//   WHEEL_SPEED_MUX1  = 0x01,
+//   WHEEL_SPEED_MUX2  = 0x02,
+// } WheelSpeedMux;
 
 #undef BUILD_ASSERT
 
--- src/publisher.cpp
+++ src/publisher.cpp
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello yaa world " << count;
+    ss << "hello - world " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
